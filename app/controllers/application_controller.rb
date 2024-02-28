fb85197422166d9ba5e6a455d9e5ef0aead5ecdf
publicover <jameslpublicover@gmail.com>
2024-02-28 23:51:45 +0100

    json web token auth system

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::API
+  include Response
+  include ExceptionHandler
+
+  before_action :authorize_request
+  attr_reader :current_user
+
+  def serialized_response(object, status = :ok)
+    serializer = Object.const_get("#{object.class.to_s.slice(/^[^:]*\s*/)}Serializer")
+    render status: status, json: serializer.new(object).serializable_hash.to_json
+  end
+
+  private
+
+    def authorize_request
+      @current_user = AuthorizeApiRequest.new(request.headers).call[:user]
+    end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,11 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
+require 'minitest/autorun'
+require 'minitest/pride'
+
+require_relative '../test/api_helper'
+
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
@@ -10,4 +15,8 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+end
+
+class Minitest::Test
+  include ApiHelper
 end
